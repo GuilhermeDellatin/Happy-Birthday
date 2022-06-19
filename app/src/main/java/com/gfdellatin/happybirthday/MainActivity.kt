bea449b59361dfc1f9bc980455ce540e08aca6e4
GuilhermeDellatin <guidellatin@gmail.com>
2022-06-19 17:23:53 -0300

    Adding Box, and image component in UI, and change text in composable function BirthdayGreetingWithText

--- app/src/main/java/com/gfdellatin/happybirthday/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/happybirthday/MainActivity.kt
@@ -3,14 +3,18 @@ package com.gfdellatin.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gfdellatin.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(message = "Happy Birthday Gui!", from = "-from Ca")
+                    BirthdayGreetingWithText(message = "Happy Birthday Gui!", from = " -from Ca")
                 }
             }
         }
@@ -32,10 +36,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
     Column {
-        Text(text = message, fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+        )
     }
 }
 
@@ -44,8 +78,8 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     showSystemUi = true
 )
 @Composable
-fun BirthdayCardPreview() {
+fun BirthdayGreetingWithImagePreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Gui!", from = "-from Ca")
+        BirthdayGreetingWithImage(message = "Happy Birthday Gui!", from = " -from Ca")
     }
 }
